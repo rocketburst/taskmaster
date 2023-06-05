20218a9e0421be45d2cff3f03f80e8b69107405e
Rayan Kazi <rayankazi7515@gmail.com>
2023-06-05 12:55:00 -0700

    feat: improved ai prompt string

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,4 +7,5 @@ export const UPDATE_ACTION_STRING =
 export const DELETE_ACTION_STRING =
   "databases.*.collections.*.documents.*.delete";
 
-export const PROMPT_STRING = `I have a couple tasks I need to do. They are listed below along with what priority they have. Can you summarize them into a neat and concise sentence? Can you make sure the tasks are mentioned by order of priority (highest to lowest) without mentioning the tasks' priorities in your response?`;
+export const PROMPT_STRING = `I have a couple tasks I need to do. They are listed below along with what priority they have. Can you summarize them into a neat and concise sentence? Can you make sure the tasks are mentioned by order of priority (highest to lowest) without mentioning the tasks' priorities in your response? If there are too many tasks in one particular category of priority, only mention in your response the tasks that are most important to an average person in that particular category. Make sure that your response is a neat concise sentence and that the response doesn't mention the task priorities (i.e. don't say that the task is a particular priority in your response, but put the task in the proper order).
+`;
