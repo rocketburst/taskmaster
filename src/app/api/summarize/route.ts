277233072a930b29fef1eb2eaced7875e65b9b88
Rayan Kazi <rayankazi7515@gmail.com>
2023-06-05 20:17:47 -0700

    fix: uncommented openai request

--- src/app/api/summarize/route.ts
+++ src/app/api/summarize/route.ts
@@ -8,9 +8,7 @@ import { AIInputSchema, type OpenAIResponse } from "@/lib/validators";
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
   analytics: true,
-  // TODO: uncomment when done
-  // limiter: Ratelimit.slidingWindow(5, "86400s"),
-  limiter: Ratelimit.slidingWindow(5, "100s"),
+  limiter: Ratelimit.slidingWindow(5, "3600s"),
 });
 
 export async function POST(req: NextRequest) {
@@ -30,41 +28,18 @@ export async function POST(req: NextRequest) {
     });
   }
 
-  // TODO: uncomment when in final and add rate limitng
-  // const summaryObject = (await fetch("https://api.openai.com/v1/completions", {
-  //   method: "POST",
-  //   headers: {
-  //     "content-type": "application/json",
-  //     Authorization: `Bearer ${env.NEXT_PUBLIC_OPENAI_API_KEY}`,
-  //   },
-  //   body: JSON.stringify({
-  //     model: "text-davinci-003",
-  //     prompt: inputString,
-  //     max_tokens: 1024,
-  //   }),
-  // }).then(res => res.json())) as OpenAIResponse;
-
-  const summaryObject = {
-    id: "cmpl-7LdZxaHICSn7Z3R1sEeMy4CULydOM",
-    object: "text_completion",
-    created: 1685391385,
-    model: "text-davinci-003",
-    choices: [
-      {
-        text: "Complete the dishes, wash the car.",
-        index: 0,
-        // logprobs: null,
-        finish_reason: "stop",
-      },
-    ],
-    usage: {
-      prompt_tokens: 73,
-      completion_tokens: 28,
-      total_tokens: 101,
+  const summaryObject = (await fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      Authorization: `Bearer ${env.NEXT_PUBLIC_OPENAI_API_KEY}`,
     },
-  } as OpenAIResponse;
-
-  console.log(remaining);
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: inputString,
+      max_tokens: 1024,
+    }),
+  }).then(res => res.json())) as OpenAIResponse;
 
   return NextResponse.json({ summaryObject, remainingUses: remaining });
 }
